66e815014b067ad6827c8e8d5b7a2466454744b6
asuka <312856403@qq.com>
2021-10-21 20:01:06 +0800

    [Bug] Fix date condition

--- src/observer/storage/common/condition_filter.cpp
+++ src/observer/storage/common/condition_filter.cpp
@@ -83,6 +83,75 @@ RC DefaultConditionFilter::init(Table &table, const Condition &condition)
     left.is_attr = false;
     left.value = condition.left_value.data;  // 校验type 或者转换类型
     type_left = condition.left_value.type;
+    if (table.table_meta().field(condition.right_attr.attribute_name)->type() == DATES) {
+        type_left = DATES;
+    }
+
+    if (type_left == DATES) {
+        char *data = static_cast<char *>(left.value);
+        std::string new_value;
+        char *token = strtok(data, "-");
+        std::vector<std::string> vec;
+        while (token != nullptr) {
+            if (strlen(token) == 1) {
+                new_value += "0";
+            }
+            vec.emplace_back(token);
+            new_value += token;
+            token = strtok(nullptr, "-");
+            if (token != nullptr) {
+                new_value += "-";
+            }
+        }
+        // check validation
+        int year = atoi(vec[0].c_str());
+        if (year <= 0) {
+            return INVALID_ARGUMENT;
+        }
+        int month = atoi(vec[1].c_str());
+        if (month <= 0 || month >= 13) {
+            return INVALID_ARGUMENT;
+        }
+        int day = atoi(vec[2].c_str());
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12: {
+                if (day <= 0 || day >= 32) {
+                    return INVALID_ARGUMENT;
+                }
+                break;
+            }
+            case 4:
+            case 6:
+            case 9:
+            case 11: {
+                if (day <= 0 || day >= 31) {
+                    return INVALID_ARGUMENT;
+                }
+                break;
+            }
+            case 2: {
+                if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                    if (day <= 0 || day >= 30) {
+                        return INVALID_ARGUMENT;
+                    }
+                } else {
+                    if (day <= 0 || day >= 29) {
+                        return INVALID_ARGUMENT;
+                    }
+                }
+                break;
+            }
+            default:
+                return INVALID_ARGUMENT;
+        }
+        left.value = (void *)new_value.c_str();
+    }
 
     left.attr_length = 0;
     left.attr_offset = 0;
@@ -104,6 +173,72 @@ RC DefaultConditionFilter::init(Table &table, const Condition &condition)
     right.is_attr = false;
     right.value = condition.right_value.data;
     type_right = condition.right_value.type;
+
+      if (type_left == DATES) {
+          char *data = static_cast<char *>(right.value);
+          std::string new_value;
+          char *token = strtok(data, "-");
+          std::vector<std::string> vec;
+          while (token != nullptr) {
+              if (strlen(token) == 1) {
+                  new_value += "0";
+              }
+              vec.emplace_back(token);
+              new_value += token;
+              token = strtok(nullptr, "-");
+              if (token != nullptr) {
+                  new_value += "-";
+              }
+          }
+          // check validation
+          int year = atoi(vec[0].c_str());
+          if (year <= 0) {
+              return INVALID_ARGUMENT;
+          }
+          int month = atoi(vec[1].c_str());
+          if (month <= 0 || month >= 13) {
+              return INVALID_ARGUMENT;
+          }
+          int day = atoi(vec[2].c_str());
+          switch (month) {
+              case 1:
+              case 3:
+              case 5:
+              case 7:
+              case 8:
+              case 10:
+              case 12: {
+                  if (day <= 0 || day >= 32) {
+                      return INVALID_ARGUMENT;
+                  }
+                  break;
+              }
+              case 4:
+              case 6:
+              case 9:
+              case 11: {
+                  if (day <= 0 || day >= 31) {
+                      return INVALID_ARGUMENT;
+                  }
+                  break;
+              }
+              case 2: {
+                  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+                      if (day <= 0 || day >= 30) {
+                          return INVALID_ARGUMENT;
+                      }
+                  } else {
+                      if (day <= 0 || day >= 29) {
+                          return INVALID_ARGUMENT;
+                      }
+                  }
+                  break;
+              }
+              default:
+                  return INVALID_ARGUMENT;
+          }
+          right.value = (void *)new_value.c_str();
+      }
 
     right.attr_length = 0;
     right.attr_offset = 0;
